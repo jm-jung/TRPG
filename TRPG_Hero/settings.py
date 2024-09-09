606d920f831b81d192413ebb4287a638d74259a0
jm-jung <jmsk7554@gmail.com>
2024-09-10 00:27:53 +0900

    Update settings.py

--- TRPG_Hero/settings.py
+++ TRPG_Hero/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-00-l7qd&h9$0s#ri%9*wc*iq_i2o$wyf#7k)vi%dml)8s=^vl+'
+SECRET_KEY = '*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
